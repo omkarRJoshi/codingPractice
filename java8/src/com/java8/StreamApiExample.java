aa6ea5c73e3f8547629c429506bb93c183ffda17
omkarRJoshi <omkarjoshi9545@gmail.com>
2022-12-06 23:30:02 +0530

    map function with stream api

--- java8/src/com/java8/StreamApiExample.java
+++ java8/src/com/java8/StreamApiExample.java
@@ -11,7 +11,11 @@ public class StreamApiExample {
 		
 		data.forEach(n -> System.out.println(n));
 		
-		data.forEach(n -> System.out.println(n));
+//		data.forEach(n -> System.out.println(n));
+//		we can't consume stream twice, it'll throw java.lang.IllegalStateException:
+		
+		Stream<Integer> doubledValues = nums.stream().map(n -> n * 2);
+		doubledValues.forEach(n -> System.out.println(n));
 
 	}
 
